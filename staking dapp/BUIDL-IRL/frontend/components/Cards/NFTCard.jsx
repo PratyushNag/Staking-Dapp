8447be4731a25330e53603e08de49c98f2700aa1
PratyushNag <pratyushnag@hotmail.com>
2023-03-26 18:47:36 +0530

    update Staking Dapp

---
+++ staking dapp/BUIDL-IRL/frontend/components/Cards/NFTCard.jsx
@@ -0,0 +1,126 @@
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import NFTAbi from "@/ABIs/BuidlNFT.json";
+import StakingAbi from "@/ABIs/Staking.json";
+import { useAccount, useContract, useSigner } from "wagmi";
+
+const NFTCard = ({ nfts, stake, tokenId }) => {
+
+  const [nfts, setNfts] = useState([]);
+  const { address } = useAccount();
+  const { data: signer } = useSigner();
+
+  const [nft, setNft] = useState({
+    name: "",
+    image: "",
+    desc: "",
+    tokenID: tokenId,
+  });
+
+  const stakingContract = useContract({
+    address: StakingAbi.address,
+    abi: StakingAbi.abi,
+    signerOrProvider: signer,
+  });
+  const nftContract = useContract({
+    address: NFTAbi.address,
+    abi: NFTAbi.abi,
+    signerOrProvider: signer,
+  });
+
+  const stakeNft = async () => {
+    try {
+      const approve = await nftContract?.isApprovedForAll(
+        address,
+        StakingAbi.address
+      );
+      console.log(approve);
+      if (!approve) {
+        const tx1 = await nftContract?.setApprovalForAll(
+          StakingAbi.address,
+          true
+        );
+      }
+      setTimeout(async () => {
+        const tx = await stakingContract?.stakeNFT(nft.tokenID);
+        console.log(tx);
+        window.alert("NFT Stake Successful");
+      }, 2000);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const unStakeNft = async () => {
+    try {
+      const tx = await stakingContract?.unStakeNFT(nft.tokenID);
+      console.log(tx);
+      const approve = await nftContract?.setApprovalForAll(
+        StakingAbi.address,
+        false
+      );
+      console.log(tx);
+      window.alert("NFT Unstake Successful");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const tx1 = await nftContract?.balanceOf(address);
+        const index = tx1.toNumber();
+        for (let i = 0; i < index; i++) {
+          const res = await fetch(nfts.url);
+          const data = await res.json();
+        }
+        const res = await fetch(nfts.url);
+        const data = await res.json();
+
+        setNft({
+          name: data.name,
+          image: data.image,
+          desc: data.description,
+          tokenID: tokenId,
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    };
+  }, [tokenId, nfts.url]);
+
+  return (
+    <div>
+      {stake ? (
+        <div>
+          <section className="text-center max-w-fit border px-3  rounded-md border-[#ffffff82] shadow-lg mx-2 hover:scale-105">
+            <h2 className="text-2xl my-2">{nft.name}</h2>
+            <Image src={nft.image ? nft.image : "/deafult.jpg"} alt={nft.name} width={200} height={400} />
+            <h2 className="text-md text-[#ffffffbe] mt-2">{nft.desc}</h2>
+            <button
+              className="bg-[#524ffffb] px-3 py-1 my-3 rounded-md font-medium mb-3 w-[60%] text-lg hover:scale-105"
+              onClick={stakeNft}
+            >
+              Stake
+            </button>
+          </section>
+        </div>
+      ) : (
+        <div>
+          <section className="text-center max-w-fit border px-3  rounded-md border-[#ffffff82] shadow-lg hover:scale-105">
+            <h2 className="text-2xl my-2">{nft.name}</h2>
+            <Image src={nft.image ? nft.image : "/deafult.jpg"} alt={nft.name} width={200} height={400} />
+            <h2 className="text-md text-[#ffffffbe] mt-2">{nft.desc}</h2>
+            <button
+              className="bg-[#ff0909] px-3 py-1 my-3 rounded-md font-medium mb-3 w-[60%] text-lg hover:scale-105"
+              onClick={unStakeNft}
+            >
+              Withdraw
+            </button>
+          </section>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default NFTCard;
